9b34f6d5a470b62f67d641b1a48c0bd47f49be22
Hori Godai <ess_president@me.com>
2018-05-08 18:14:24 +0900

    deploy環境を用意

---
+++ deploy.js
@@ -0,0 +1,73 @@
+const cmd = require('node-cmd');
+const fs = require('fs-extra');
+const co = require('co');
+const archiver = require('archiver');
+const pkg = fs.readJsonSync('./package.json');
+
+/**
+ * Run system command
+ *
+ * @param cmdString
+ * @returns {Promise}
+ */
+const systemCmd = cmdString => {
+  return new Promise((resolve) => {
+    cmd.get(
+      cmdString,
+      (data, err, stderr) => {
+        console.log(cmdString);
+        console.log(data);
+        if (err) {
+          console.log(err);
+        }
+        if (stderr) {
+          console.log(stderr);
+        }
+        resolve(data);
+      }
+    );
+  });
+}
+
+const zipPromise = (src, dist) => {
+  return new Promise((resolve, reject) => {
+    const archive = archiver.create('zip', {});
+    const output = fs.createWriteStream(dist);
+
+    // listen for all archive data to be written
+    output.on('close', () => {
+      console.log(archive.pointer() + ' total bytes');
+      console.log('Archiver has been finalized and the output file descriptor has closed.');
+      resolve();
+    });
+
+    // good practice to catch this error explicitly
+    archive.on('error', (err) => {
+      reject(err);
+    });
+
+    archive.pipe(output);
+    archive.directory(src).finalize();
+  });
+}
+
+co(function* () {
+  try {
+    fs.mkdirsSync(`build`);
+    fs.mkdirsSync(`build/28x`);
+    yield zipPromise(`28x/cpi`, `./build/28x/cpi.zip`);
+    yield zipPromise(`28x/heteml`, `./build/28x/heteml.zip`);
+    yield zipPromise(`28x/lolipop`, `./build/28x/lolipop.zip`);
+    yield zipPromise(`28x/mamp`, `./build/28x/mamp.zip`);
+    yield zipPromise(`28x/sakura`, `./build/28x/sakura.zip`);
+    yield zipPromise(`28x/xampp`, `./build/28x/xampp.zip`);
+    yield zipPromise(`28x/xserver`, `./build/28x/xserver.zip`);
+    yield zipPromise(`28x/zenlogic`, `./build/28x/zenlogic.zip`);
+    yield zipPromise(`28x/update`, `./build/28x/update.zip`);
+    yield systemCmd('git add -A');
+    yield systemCmd(`git commit -m "v${pkg.version}"`);
+    yield systemCmd('git push');
+  } catch (err) {
+    console.log(err);
+  }
+});
